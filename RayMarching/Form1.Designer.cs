dce4952bbea668fcdb6eb2772936291bdbfaa158
Gekeris <you@example.com>
2020-06-07 15:26:06 +0300

    Random camera fill

--- RayMarching/Form1.Designer.cs
+++ RayMarching/Form1.Designer.cs
@@ -28,36 +28,77 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.Camera1 = new System.Windows.Forms.PictureBox();
+			this.SaveCheckBox = new System.Windows.Forms.CheckBox();
+			this.ResetButton = new System.Windows.Forms.Button();
+			this.UpdateButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.Camera1)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// pictureBox1
+			// Camera1
 			// 
-			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(640, 480);
-			this.pictureBox1.TabIndex = 0;
-			this.pictureBox1.TabStop = false;
+			this.Camera1.Location = new System.Drawing.Point(0, 0);
+			this.Camera1.Name = "Camera1";
+			this.Camera1.Size = new System.Drawing.Size(640, 480);
+			this.Camera1.TabIndex = 0;
+			this.Camera1.TabStop = false;
+			// 
+			// SaveCheckBox
+			// 
+			this.SaveCheckBox.AutoSize = true;
+			this.SaveCheckBox.Location = new System.Drawing.Point(646, 12);
+			this.SaveCheckBox.Name = "SaveCheckBox";
+			this.SaveCheckBox.Size = new System.Drawing.Size(51, 17);
+			this.SaveCheckBox.TabIndex = 1;
+			this.SaveCheckBox.Text = "Save";
+			this.SaveCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// ResetButton
+			// 
+			this.ResetButton.Location = new System.Drawing.Point(780, 445);
+			this.ResetButton.Name = "ResetButton";
+			this.ResetButton.Size = new System.Drawing.Size(92, 23);
+			this.ResetButton.TabIndex = 2;
+			this.ResetButton.Text = "Reset to default";
+			this.ResetButton.UseVisualStyleBackColor = true;
+			this.ResetButton.Click += new System.EventHandler(this.DefaultSettings);
+			// 
+			// UpdateButton
+			// 
+			this.UpdateButton.Location = new System.Drawing.Point(646, 445);
+			this.UpdateButton.Name = "UpdateButton";
+			this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+			this.UpdateButton.TabIndex = 3;
+			this.UpdateButton.Text = "Update";
+			this.UpdateButton.UseVisualStyleBackColor = true;
+			this.UpdateButton.Click += new System.EventHandler(this.UpdateCamera);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(884, 480);
-			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.UpdateButton);
+			this.Controls.Add(this.ResetButton);
+			this.Controls.Add(this.SaveCheckBox);
+			this.Controls.Add(this.Camera1);
 			this.MaximumSize = new System.Drawing.Size(900, 519);
 			this.MinimumSize = new System.Drawing.Size(900, 519);
 			this.Name = "Form1";
 			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.Camera1)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox Camera1;
+		private System.Windows.Forms.CheckBox SaveCheckBox;
+		private System.Windows.Forms.Button ResetButton;
+		private System.Windows.Forms.Button UpdateButton;
 	}
 }
 
--- RayMarching/Form1.cs
+++ RayMarching/Form1.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Drawing.Text;
 
 namespace RayMarching
 {
 	public partial class Form1 : Form
 	{
+		const int CameraW = 640;
+		const int CameraH = 480;
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			if (!Directory.Exists(".\\Pictures"))
+				Directory.CreateDirectory(".\\Pictures");
+			DefaultSettings(sender, e);
+			UpdateCamera(sender, e);
+		}
+
+		private void DefaultSettings(object sender, EventArgs e)
+		{
+			SaveCheckBox.Checked = false;
+		}
+
+		private void UpdateCamera(object sender, EventArgs e)
+		{
+			Camera1.Image = CreateBitmap();
+		}
+
+		private Bitmap CreateBitmap()
+		{
+			Bitmap bitmap = new Bitmap(CameraW, CameraH);
+			Random rand = new Random();
+			for (int y = 0; y < CameraH; y++)
+			{
+				for (int x = 0; x < CameraW; x++)
+				{
+					int red = rand.Next(256);
+					int green = rand.Next(256);
+					int blue = rand.Next(256);
+
+					bitmap.SetPixel(x, y, Color.FromArgb(red, green, blue));
+				}
+			}
+			if (SaveCheckBox.Checked)
+			{
+				DateTime thisDay = DateTime.Now;
+				string path = ".\\Pictures\\" + thisDay.Year + "_" + ZeroCheck(thisDay.Month) + ZeroCheck(thisDay.Day) + ZeroCheck(thisDay.Hour) + ZeroCheck(thisDay.Minute) + ZeroCheck(thisDay.Second);
+				for(int i = 0; i >=0; i++)
+				{
+					if (!File.Exists(path + i + ".bmp"))
+					{
+						path += i + ".bmp";
+						break;
+					}
+				}
+				File.WriteAllText(path, "");
+				bitmap.Save(path);
+
+				string ZeroCheck(int num)
+				{
+					if (num < 10)
+						return "0" + num + "_";
+					else
+						return num + "_";
+				}
+			}
+			return bitmap;
 		}
 	}
 }
